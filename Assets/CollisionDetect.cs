85910b4f36b7cc5e677bd6a7cedf5f2b1e2f70b5
kwabbott <31709175+kwabbott@users.noreply.github.com>
2018-07-23 15:00:59 -0400

    Initial commit

---
+++ Assets/CollisionDetect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CollisionDetect : MonoBehaviour {
+
+	// Use this for initialization
+	private void OnParticleCollision(GameObject other)
+	{
+		Debug.Log(other.name);
+	}
+
+
+	
+	
+}
